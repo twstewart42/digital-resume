abb5255d1b9e3dc3353d6f81fdafa9fafdbfd8b1
Tom Stewart <twstewart42@gmail.com>
2019-07-26 15:11:45 -0400

    terraform this site

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+ 
+module "cloudfront-s3-website" {
+    source           = "chgangaraju/cloudfront-s3-website/aws"
+    version          = "1.0.0"
+    hosted_zone      = "twstewart.me"
+    domain_name      = "twstewart.me"
+    aws_region       = "us-east-1"
+}
